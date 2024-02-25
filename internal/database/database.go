f7bbba7d28f6183af7d4adf69037eb0c99f9ebaa
Mart1n-Eden <lexma9229@gmail.com>
2024-02-25 18:18:09 +0300

    Initial commit

---
+++ internal/database/database.go
@@ -0,0 +1,34 @@
+package database 
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "postgres"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Init(entity interface{}) {
+	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	var err error
+
+	db,err = gorm.Open(postgres.Open(dbinfo),  &gorm.Config{})
+	if  err != nil {
+		panic(err)
+	}
+
+	err = db.AutoMigrate(entity)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/server/app.go
@@ -0,0 +1,75 @@
+package server
+
+import (
+	"net/http"
+	"encoding/json"
+	"math/rand"
+	"github.com/google/uuid"
+
+	"github.com/Mart1n-Eden/randomgen/internal/database"
+)
+
+type GenRequest struct {
+	Type   string `json:"type"`
+	Length int    `json:"length"`
+}
+
+type GenResponse struct {
+	ID  string `json:"id"`
+	Val string    `json:"value"`
+}
+
+func Generate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req GenRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	}
+
+	value := req.GenValue()
+
+	res := GenResponse {
+		ID: uuid.NewString(),
+		Val: value,
+	}
+
+	res.PlaceDB()
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func (p *GenRequest) GenValue() string {
+	var charset string
+	switch p.Type {
+	case "alpha":
+		charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	case "alphanumeric":
+		charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	case "numeric":
+		charset = "0123456789"
+	case "guid":
+		charset = "abcdef0123456789"
+	default:
+		charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	}
+
+	value := make([]byte, p.Length)
+	for i := range value {
+		value[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(value)
+}
+
+func (p *GenResponse) PlaceDB() {
+	db.Create(p)
+}
+
+func Retrieve(w http.ResponseWriter, r *http.Request) {
+
+}
